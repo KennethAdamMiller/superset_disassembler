b2f014f32cabfa393011f2b0a8b37b5a51a99af9
Kenneth Adam Miller <kennethadammiller@gmail.com>
2019-01-05 07:05:49 -0500

    Apply some fixes to rq3.sh

--- scripts/rq3.sh
+++ scripts/rq3.sh
@@ -12,20 +12,19 @@ fn_check() {
   objdump --no-show-raw-insn -d ${o}_stripped | grep ":" | cut -d ":" -f 1 | grep -e "[0-9a-f]"     | grep -v "[g-z]" > ${o}.lserr
   #fi
   if [ ! -f "${o}_stripped_addrs.txt" ]; then
-      superset_disasm --target=${o}_stripped --save_addrs --tp_threshold=0.99
+      ./superset_disasm.native --target=${o}_stripped --ground_truth_bin=${f} --save_addrs --tp_threshold=0.99
       mv $(basename ${f})_stripped_addrs.txt ${RQ3_folder}/
   fi
   if [ ! -f "${o}.truth" ]; then
-      superset_disasm --target=${f} --ground_truth_bin=${f} --tp_threshold=0.99 --save_gt > ${o}.truth
+      ./superset_disasm.native --target=${f} --ground_truth_bin=${f} --tp_threshold=0.99 --save_gt > ${o}.truth
   fi
   echo "$(basename ${f}) results: " 
-  python ${HOME}/workspace/superset_disassembler/scripts/calc_fn.py ${o}.truth ${o}.lserr ${o}_stripped_addrs.txt
+  python ${HOME}/workspace/superset_disasm/scripts/calc_fn.py ${o}.truth ${o}.lserr ${o}_stripped_addrs.txt
   echo ""
 }
 
 fn_check ${RQ3_folder}/gcc_coreutils_64_O0_chroot
 fn_check ${RQ3_folder}/gcc_findutils_64_O0_xargs
-rm ${RQ3_folder}/gcc_coreutils_64_O0_make-prime-list_stripped_addrs.txt
 fn_check ${RQ3_folder}/gcc_coreutils_64_O0_make-prime-list
 fn_check ${RQ3_folder}/gcc_coreutils_64_O0_true
 fn_check ${RQ3_folder}/gcc_coreutils_64_O0_false
