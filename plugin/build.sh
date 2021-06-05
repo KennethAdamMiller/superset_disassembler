07f7035c10358e425c8c52e443de03a1ac9c52b4
Kenneth Adam Miller <kennethadammiller@gmail.com>
2021-06-04 20:46:28 -0400

    Move build to plugin folder

--- plugin/build.sh
+++ plugin/build.sh
@@ -1,5 +1,5 @@
-pushd plugin/
+#pushd plugin/
 rm -rf _build
 #-pkg superset_disassemblers 
 bapbuild -pkg bap-primus -pkg bap-knowledge -pkg superset_disassemblers superset_disassembler.plugin
-popd
+#popd
