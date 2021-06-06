3b151d130810e5629b93cf99f5a1d45474a59e25
Kenneth Adam Miller <kennethadammiller@gmail.com>
2021-06-05 23:25:41 -0400

    Update dependencies, update to bap-future, add src/streams

---
+++ src/streams.ml
@@ -0,0 +1,17 @@
+open Core_kernel
+open Bap.Std
+open Bap_future.Std
+
+(* disasm *)
+(* lifting *)
+(* invariants *)
+(* analyses *)
+(* features (recognition, registration, execution) *)
+(* convergence *)
+(* mark bad, removal *)
+(* ground truth *)
+(* metrics (different sets; occlusion, ro, tps, fps *)
+(* report *)
+(* traversal *)
+(* trimming *)
+(* output *)
