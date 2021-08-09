ea4817adc03680fcbf136fed10f58b4b41e4f0da
Kenneth Adam Miller <kennethadammiller@gmail.com>
2021-08-08 22:32:42 -0400

    Update flambda dockerfile and script
    +Update bap default to 2.3.0
    +Move late dependencies (time, parallel) further up in dockerfile
    +Due to build failures, limit opam jobs to one.

--- dockerFlambda.sh
+++ dockerFlambda.sh
@@ -2,7 +2,7 @@ echo "Usage: $0"
 TAG=$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short HEAD)
 echo "TAG=${TAG}"
 BAPVERSION=${1}
-if [ -z ${1} ]; then BAPVERSION=2.1.0; fi
+if [ -z ${1} ]; then BAPVERSION=2.3.0; fi
 
 sudo docker build . -f Dockerfile.flambda \
      --build-arg TAG=${TAG} \
