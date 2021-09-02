b6737495439f24eb460d75fbc511506c3bebc2e3
Kenneth Adam Miller <kennethadammiller@gmail.com>
2021-09-02 10:19:35 -0400

    Update run_tests.sh to run unit tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,5 @@
 #this should run docker or docker should call this?
+make clean && make && ./test_superset_disasm.native
 ./functional_tests/test_cache.sh > cache_tests.txt
 cat cache_tests.txt
 ./functional_tests/test_cmdline.sh > cmdline_tests.txt
