6ba76cd9617165ba316660d76c7e80fb2c8cc97a
Kenneth Adam Miller <kennethadammiller@gmail.com>
2021-09-07 18:48:48 -0400

    Update read_cache to closely reflect arguments to plugin

--- scripts/read_cache.sh
+++ scripts/read_cache.sh
@@ -1,4 +1,4 @@
 read_cache() {
-    echo "bap superset_distribution --metrics $@"
+    echo "bap superset_distribution --metrics \"ro: %1\",\"reduced_occlusion\" $@"
     bap superset_distribution --metrics "ro: %1","reduced_occlusion" $@
 }
