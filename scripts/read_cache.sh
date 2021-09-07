245d7b2f792c0398fa4dc273feed5c2a919cb86c
Kenneth Adam Miller <kennethadammiller@gmail.com>
2021-09-07 19:59:25 -0400

    Print occlusive space

--- scripts/read_cache.sh
+++ scripts/read_cache.sh
@@ -1,4 +1,4 @@
 read_cache() {
-    echo "bap superset_distribution --metrics \"ro: %1\",\"superset-disasm-metrics:reduced_occlusion\" $@"
-    bap superset_distribution --metrics "ro: %1","superset-disasm-metrics:reduced_occlusion" $@
+    echo "bap superset_distribution --metrics \"ro: %1\",\"superset-disasm-metrics:reduced_occlusion\",\"superset_disasm-metrics:occlusives_space\" $@"
+    bap superset_distribution --metrics "ro: %1 of %2","superset-disasm-metrics:reduced_occlusion","superset-disasm-metrics:occlusive_space" $@
 }
