44e7343a099d98805bcde18a1df68081a8a01cab
Kenneth Adam Miller <kennethadammiller@gmail.com>
2021-09-11 18:54:39 -0400

    Add new test_fns and some adjustments to test_cache

--- functional_tests/test_cache.sh
+++ functional_tests/test_cache.sh
@@ -7,10 +7,15 @@ export features="TrimLimitedClamped,FixpointGrammar,Liveness"
 export rounds=3
 export gt_bin="${HOME}/workspace/x86-binaries/elf/findutils/gcc_findutils_32_O0_bigram"
 args="${gt_bin} --ground_truth_bin=${gt_bin} "
-suff+="--metrics \"tps: %1, fns: %2\",\"superset-disasm-metrics:true_positives\",\"superset-disasm-metrics:false_negatives\""
+suff+=" --invariants=\"\""
+suff+=" --analyses=\"\""
+suff+=" --features=\"\""
 echo "Executing for ground truth"
-echo "time bap superset_distribution --u ${args} ${suff} "
-time bap superset_distribution --u ${args} ${suff} 
+echo "time bap superset_disasm --u ${args} ${suff} "
+time bap superset_disasm --u ${args} ${suff}
+echo "\n"
+suff=="--metrics \"tps: %1, fns: %2\",\"superset-disasm-metrics:true_positives\",\"superset-disasm-metrics:false_negatives\" "
+time bap superset_distribution ${args} ${suff} 
 if [[ ! (-z ${phases}) ]]; then
     args+=" --invariants=${phases}"
 fi
--- functional_tests/test_fns.sh
+++ functional_tests/test_fns.sh
@@ -1,1 +1,22 @@
 echo "=====$0====="
+eval $(opam config env)
+source scripts/read_cache.sh
+#export SUPERSET_FRONTEND=1
+export features="TrimLimitedClamped,FixpointGrammar,Liveness"
+export rounds=3
+export gt_bin="${HOME}/workspace/x86-binaries/elf/findutils/gcc_findutils_32_O0_bigram"
+args="${gt_bin} --ground_truth_bin=${gt_bin} --tp_threshold=0.20 "
+if [[ ! (-z ${phases}) ]]; then
+    args+=" --invariants=${phases}"
+fi
+if [[ ! (-z ${analyses}) ]]; then
+    args+=" --analyses=${analyses}"
+fi
+if [[ ! (-z ${features}) ]]; then
+    args+=" --features=${features}"
+fi
+args+="--rounds=${rounds} "
+echo "time bap superset_disasm --u ${args}"
+time bap superset_disasm --u ${args}
+
+bap superset_distribution --metrics "%1","superset-disasm-metrics:false_negatives" ${args}
