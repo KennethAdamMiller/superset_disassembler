d59e339d7369d1e6790fec74abc45c8568dc42d4
Kenneth Adam Miller <kennethadammiller@gmail.com>
2021-09-11 18:10:16 -0400

    Correct some typo in bap plugin command functional tests
    +add new functional tests for false negatives and reports

--- functional_tests/test_cache.sh
+++ functional_tests/test_cache.sh
@@ -7,9 +7,10 @@ export features="TrimLimitedClamped,FixpointGrammar,Liveness"
 export rounds=3
 export gt_bin="${HOME}/workspace/x86-binaries/elf/findutils/gcc_findutils_32_O0_bigram"
 args="${gt_bin} --ground_truth_bin=${gt_bin} "
+suff+="--metrics \"tps: %1, fns: %2\",\"superset-disasm-metrics:true_positives\",\"superset-disasm-metrics:false_negatives\""
 echo "Executing for ground truth"
-echo "time bap superset_disasm --u ${args}"
-time bap superset_disasm --u ${args}
+echo "time bap superset_distribution --u ${args} ${suff} "
+time bap superset_distribution --u ${args} ${suff} 
 if [[ ! (-z ${phases}) ]]; then
     args+=" --invariants=${phases}"
 fi
@@ -20,7 +21,6 @@ if [[ ! (-z ${features}) ]]; then
     args+=" --features=${features}"
 fi
 args+="--rounds=${rounds} "
-
 echo "time bap superset_disasm --u ${args}"
 time bap superset_disasm --u ${args}
 
