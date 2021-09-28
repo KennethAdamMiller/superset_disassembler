9ddc585c580f848aaf5928938dcb4cebaf3e44e6
Kenneth Adam Miller <kennethadammiller@gmail.com>
2021-09-28 11:34:17 -0500

    Tested install_malamute to working

--- scripts/install_malamute.sh
+++ scripts/install_malamute.sh
@@ -1,12 +1,18 @@
-git clone git://github.com/jedisct1/libsodium.git
+#!/bin/bash
+x=$(pwd)
+cd /tmp/
+sudo apt-get update && sudo apt-get install libtool autoconf automake -y
+git clone --branch stable git://github.com/jedisct1/libsodium.git
 git clone git://github.com/zeromq/libzmq.git
 git clone git://github.com/zeromq/czmq.git
 git clone git://github.com/zeromq/malamute.git
 for project in libsodium libzmq czmq malamute; do
     cd $project
+    echo $(pwd)
     ./autogen.sh
-    ./configure && make check
+    ./configure && make check -j$(nproc --a)
     sudo make install
     sudo ldconfig
     cd ..
 done
+cd ${x}
