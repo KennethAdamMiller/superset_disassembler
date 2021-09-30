6c85dae4745cbd30b40e6712482978abb5ea165a
Kenneth Adam Miller <kennethadammiller@gmail.com>
2021-09-30 12:00:57 -0500

    Filter the summaries to remove option

--- plugin/superset_disassembler.ml
+++ plugin/superset_disassembler.ml
@@ -581,22 +581,31 @@ let mat_of_list l =
 
 let make_plots summaries =
   let open Metrics in
+  let summaries = List.filter_map summaries ~f:(fun s ->
+                      match s.zie, s.occ, s.occ_space, s.fns, s.fps,
+                            s.tps with
+                      | None, _, _, _, _, _ -> None
+                      | _, None, _, _, _, _ -> None
+                      | _, _, None, _, _, _ -> None
+                      | _, _, _, None, _, _ -> None
+                      | _, _, _, _, None, _ -> None
+                      | _, _, _, _, _, None -> None
+                      | _ -> Some s
+                    ) in
   let sz_occ = Plot.create "size_and_occlusion.png" in
   let occ_occspace = Plot.create "occlusion_and_occspace.png" in
   let occcnt_occfuncs  = Plot.create "occcnt_occfuncs.png" in
   let size_time = Plot.create "size_time.png" in
   let safe_conv = Plot.create "safe_conv.png" in
   let occr_numbins = Plot.create "occr_numbins.png" in
-  let sizes = Option.all @@ List.map summaries ~f:(fun s -> s.size) in
-  let occ = Option.all @@ List.map summaries ~f:(fun s -> s.occ) in
-  let occ_space = Option.all @@
-                    List.map summaries ~f:(fun s -> s.occ_space) in
-  let fe = Option.all @@ List.map summaries ~f:(fun s -> s.fe) in
-  let clean = Option.all @@
-                List.map summaries ~f:(fun s -> s.occ_space) in
-  let fns = Option.all @@ List.map summaries ~f:(fun s -> s.fns) in
-  let fps = Option.all @@ List.map summaries ~f:(fun s -> s.fps) in
-  let tps = Option.all @@ List.map summaries ~f:(fun s -> s.tps) in
+  let sizes = List.map summaries ~f:(fun s -> s.size) in
+  let occ = List.map summaries ~f:(fun s -> s.occ) in
+  let occ_space = List.map summaries ~f:(fun s -> s.occ_space) in
+  let fe = List.map summaries ~f:(fun s -> s.fe) in
+  let clean = List.map summaries ~f:(fun s -> s.occ_space) in
+  let fns = List.map summaries ~f:(fun s -> s.fns) in
+  let fps = List.map summaries ~f:(fun s -> s.fps) in
+  let tps = List.map summaries ~f:(fun s -> s.tps) in
   Plot.scatter ~h:sz_occ (mat_of_list sizes) (mat_of_list occ);
   Plot.output sz_occ;
   Plot.scatter ~h:occ_occspace (mat_of_list occ)
