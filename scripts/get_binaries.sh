fb266860019c1d9cb3d6f9d730ad0b9d22905a25
Kenneth Adam Miller <kennethadammiller@gmail.com>
2021-11-19 12:15:33 -0500

    Revert get_binaries due to uniform docker directory structure

--- scripts/get_binaries.sh
+++ scripts/get_binaries.sh
@@ -6,4 +6,4 @@ find ${HOME}/workspace/x86-binaries/elf/findutils -type f -print  >> binaries.tx
 find ${HOME}/workspace/x86-binaries/elf/coreutils -type f -print  >> binaries.txt
 find ${HOME}/workspace/x86_64-binaries/elf/coreutils -type f -print  >> binaries.txt
 find ${HOME}/workspace/arm-binaries/coreutils -type f -print  >> binaries.txt
-python3 scripts/replace_home_path.py binaries.txt
+#python3 scripts/replace_home_path.py binaries.txt
