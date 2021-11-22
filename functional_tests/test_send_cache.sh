83369c50e2401d0970433b7264eca6e0b90e3e31
Kenneth Miller <kennethadammiller@gmail.com>
2021-11-21 22:50:44 -0500

    Syntax error in kubectl apply broker config

--- functional_tests/test_send_cache.sh
+++ functional_tests/test_send_cache.sh
@@ -1,5 +1,5 @@
 source scripts/tag.sh
 export replicas=1
-cat ../configs/analysis.yaml | envsubst | kubectl apply
+cat ./configs/analysis.yaml | envsubst | kubectl apply -f -
 export test_size=1
-cat ../configs/broker.yaml | envsubst | kubectl apply
+cat ./configs/broker.yaml | envsubst | kubectl apply -f -
