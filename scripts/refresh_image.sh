c50d9b5388e7caca2ab131b50b535545241e99f5
Kenneth Miller <kennethadammiller@gmail.com>
2021-11-24 14:38:30 -0500

    Reduce the test running time by putting a small binary at the front

--- scripts/refresh_image.sh
+++ scripts/refresh_image.sh
@@ -1,4 +1,4 @@
-source scripts/tag.h
+source scripts/tag.sh
 ./dockerFlambda.sh
 docker tag superset_disasm_flambda:${TAG} ${registry}superset_disasm_flambda:${TAG} 
 docker push ${registry}superset_disasm_flambda:${TAG}
