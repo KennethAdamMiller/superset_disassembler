56c04f4987c5d4d9d759f9fd577dd26ddbcf0100
Kenneth Miller <kennethadammiller@gmail.com>
2021-11-24 09:54:09 -0500

    Appearances seem to indicate a functioning distributed test

--- functional_tests/test_kube_communicate.sh
+++ functional_tests/test_kube_communicate.sh
@@ -1,5 +1,6 @@
 source scripts/tag.sh
 export replicas=1
-cat ./configs/analysis.yaml | envsubst | kubectl apply -f -
 export test_size=1
-cat ./configs/broker.yaml | envsubst | kubectl apply -f -
+cat ./configs/broker-deployment.yaml | envsubst | kubectl apply -f -
+cat ./configs/broker-service.yaml | envsubst | kubectl apply -f -
+cat ./configs/analysis-deployment.yaml | envsubst | kubectl apply -f -
