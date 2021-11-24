6fdda72389f62628de8c14ae593a470a1df8f120
Kenneth Miller <kennethadammiller@gmail.com>
2021-11-24 15:00:45 -0500

    Change analysis from deployment to job

--- functional_tests/test_kube_communicate.sh
+++ functional_tests/test_kube_communicate.sh
@@ -1,6 +1,6 @@
 source scripts/tag.sh
 export replicas=1
 export test_size=1
-cat ./configs/broker-deployment.yaml | envsubst | kubectl apply -f -
+cat ./configs/broker-job.yaml | envsubst | kubectl apply -f -
 cat ./configs/broker-service.yaml | envsubst | kubectl apply -f -
-cat ./configs/analysis-deployment.yaml | envsubst | kubectl apply -f -
+cat ./configs/analysis-job.yaml | envsubst | kubectl apply -f -
