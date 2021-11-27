199ce37282ffd145c2c0e6a9cfa4ed4329f59ff2
Kenneth Miller <kennethadammiller@gmail.com>
2021-11-26 20:09:45 -0500

    Add new script for entire corpora analysis

---
+++ scripts/corpora_analysis.sh
@@ -0,0 +1,12 @@
+source scripts/tag.sh
+export test_size=999999
+export replicas=5
+export cores=108
+cat ./configs/broker-job.yaml | envsubst | kubectl apply -f -
+cat ./configs/broker-service.yaml | envsubst | kubectl apply -f -
+cat ./configs/analysis-job.yaml | envsubst | kubectl apply -f -
+kubectl wait --for=condition=complete --timeout=-1s job/analysis-${TAG}
+if [[ $? -eq 0 ]]; then
+	kubectl delete service broker-service-${TAG}
+	kubectl delete job/analysis-${TAG}
+fi
