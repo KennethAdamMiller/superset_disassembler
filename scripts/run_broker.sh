a08d34f645d1f20db61da5c5a5ee8c72700ff015
Kenneth Miller <kennethadammiller@gmail.com>
2021-11-30 02:36:11 -0500

    Try commenting out bap recv

--- scripts/run_broker.sh
+++ scripts/run_broker.sh
@@ -4,7 +4,7 @@ if [[ -z ${test_size} ]]; then
 	export test_size=99999
 fi
 eval $(opam env)
-bap recv_cache --perpetuate --bind_addr="tcp://*:9996" & 
+#bap recv_cache --perpetuate --bind_addr="tcp://*:9996" & 
 recvr=$!
 echo "Starting run_broker.py"
 python3 scripts/run_broker.py ${test_size}
