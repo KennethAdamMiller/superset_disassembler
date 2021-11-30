8a9075ad39978dc12ab8e06b374c02be008afd95
Kenneth Miller <kennethadammiller@gmail.com>
2021-11-30 05:52:40 -0500

    Move subprocess command from shell to python

--- scripts/run_broker.py
+++ scripts/run_broker.py
@@ -1,12 +1,19 @@
 from distributor import dealer
+import subprocess, shlex
 import zmq
 import sys
 
 if len(sys.argv) > 1:
     tests=int(sys.argv[1])
+else:
+    tests=99999
 
+
+recv_cmd=shlex.split('bap recv_cache --perpetuate --bind_addr="tcp://*:9996"')
+recvr=subprocess.Popen(recv_cmd)
 print("creating context")
 context=zmq.Context()
 print("created context")
 b=dealer(ctxt=context, test_size=sys.argv[1])
 b.run()
+recvr.communicate()
--- scripts/run_broker.sh
+++ scripts/run_broker.sh
@@ -1,14 +1,11 @@
 ./scripts/get_binaries.sh
 export test_size=$1
-if [[ -z ${test_size} ]]; then
-	export test_size=99999
-fi
 eval $(opam env)
 echo "Starting run_broker.py"
-python3 scripts/run_broker.py ${test_size} &
-broker=$!
-bap recv_cache --perpetuate --bind_addr="tcp://*:9996" & 
-recvr=$!
-wait ${broker}
-kill ${recvr}
+python3 scripts/run_broker.py ${test_size}
+#broker=$!
+#bap recv_cache --perpetuate --bind_addr="tcp://*:9996" & 
+#recvr=$!
+#wait ${broker}
+#kill ${recvr}
 plot_superset_cache
