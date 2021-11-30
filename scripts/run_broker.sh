751a68b932d13ab5897ab3fc55c074e3d9755468
Kenneth Miller <kennethadammiller@gmail.com>
2021-11-30 05:34:38 -0500

    Try reversing bap recvr with run_broker.py

--- scripts/run_broker.sh
+++ scripts/run_broker.sh
@@ -4,11 +4,11 @@ if [[ -z ${test_size} ]]; then
 	export test_size=99999
 fi
 eval $(opam env)
-bap recv_cache --perpetuate --bind_addr="tcp://*:9996" & 
-recvr=$!
 echo "Starting run_broker.py"
 python3 scripts/run_broker.py ${test_size} &
 broker=$!
+bap recv_cache --perpetuate --bind_addr="tcp://*:9996" & 
+recvr=$!
 wait ${broker}
 kill ${recvr}
 plot_superset_cache
