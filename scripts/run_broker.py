094582e4297dab2f2f0ff5e0a63755c6cf7d8b73
Kenneth Miller <kennethadammiller@gmail.com>
2021-11-30 04:23:55 -0500

    Clarify lockup totally

--- scripts/run_broker.py
+++ scripts/run_broker.py
@@ -5,6 +5,8 @@ import sys
 if len(sys.argv) > 1:
     tests=int(sys.argv[1])
 
+print("creating context")
 context=zmq.Context()
+print("created context")
 b=dealer(ctxt=context, test_size=sys.argv[1])
 b.run()
