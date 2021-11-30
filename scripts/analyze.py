8dd68c78cf43cae12ff1758b71f35a7bf314d55d
Kenneth Miller <kennethadammiller@gmail.com>
2021-11-29 21:28:25 -0500

    Ensure that bap is on the path

--- scripts/analyze.py
+++ scripts/analyze.py
@@ -2,12 +2,14 @@ from client import worker
 import subprocess
 import sys
 import zmq
+import os
 
 def run_bap(addr, msg):
     disasm="bap superset_disasm --heuristics=TrimLimitedClamped,Liveness --ground_truth_bin={} {}"
     disasm=disasm.format(msg, msg)
     print(disasm)
-    disasm=disasm.split(" ")
+    path=os.environ["PATH"]
+    disasm=['/usr/bin/env', '-P', path] + disasm.split(" ")
     analysis=subprocess.Popen(disasm, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     analysis.communicate()
     transfer_cmd="bap send_cache {} --send_cache=tcp://" + addr + ":9996".format(msg)
