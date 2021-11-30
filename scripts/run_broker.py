1247337bb6301b1f2d54119c72db76a6d0fc6a24
Kenneth Miller <kennethadammiller@gmail.com>
2021-11-30 08:13:32 -0500

    Try starting new session

--- scripts/run_broker.py
+++ scripts/run_broker.py
@@ -13,7 +13,7 @@ context=zmq.Context()
 print("created context")
 
 recv_cmd=shlex.split('bap recv_cache --perpetuate --bind_addr="tcp://*:9996"')
-recvr=subprocess.Popen(recv_cmd)
+recvr=subprocess.Popen(recv_cmd, start_new_session=True)
 
 b=dealer(ctxt=context, test_size=sys.argv[1])
 b.run()
