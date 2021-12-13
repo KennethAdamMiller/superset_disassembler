d865f545e572a516bfc5cec9b6b1c0969a622580
Kenneth Miller <kennethadammiller@gmail.com>
2021-12-13 14:38:24 -0500

    Specify fsuffix build arg

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -4,9 +4,11 @@ if [ -z ${1} ]; then BAPVERSION=2.3.0; fi
 
 source scripts/tag.sh
 source scripts/feature_suffix.sh
-IMG_TGT=superset_disasm_${FSUFFIX}:${TAG}
+IMG_TGT=superset_disasm:${TAG}-${FSUFFIX}
 echo "Building ${IMG_TGT} with BAPVERSION=${BAPVERSION}"
 sudo docker build . -f Dockerfile \
      -t  ${IMG_TGT} \
      ${DOCKER_BLD_ARGS} \
      --build-arg BAPVERSION=${BAPVERSION} 
+
+docker tag superset_disasm:${TAG}-${FSUFFIX} superset_disasm:latest-${FSUFFIX}
