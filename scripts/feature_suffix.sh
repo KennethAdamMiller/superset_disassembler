960395678bddb104e9cf86dc1a98a9e6feb985cf
Kenneth Miller <kennethadammiller@gmail.com>
2021-12-13 14:12:44 -0500

    Correct error in feature suffix to ensure isolation in environment variables

--- scripts/feature_suffix.sh
+++ scripts/feature_suffix.sh
@@ -1,2 +1,3 @@
 #for each variable, add the variable name and value to a hash
-export FSUFFIX=$(bash -c "source ./vars.sh; echo $(env | md5sum | cut -c1-6)")
+
+export FSUFFIX=$(env -i bash -c 'excluded=$(env | LC_ALL=C sed -n "s/^\([A-Z_a-z][0-9A-Z_a-z]*\)=.*/\1/p" | tr "\n" " ") ; unset ${excluded} ; source ./vars.sh; echo $(env) | md5sum | cut -c1-6')
