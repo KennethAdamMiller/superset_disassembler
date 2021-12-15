f15c6ff98b33bf8d7e31ebddb89ed802e0fbe730
Kenneth Miller <kennethadammiller@gmail.com>
2021-12-14 22:38:15 -0500

    Thread the opam switch var through to docker

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -4,11 +4,12 @@ if [ -z ${1} ]; then BAPVERSION=2.3.0; fi
 
 source scripts/tag.sh
 source scripts/feature_suffix.sh
+source vars.sh
 IMG_TGT=superset_disasm:${TAG}-${FSUFFIX}
 echo "Building ${IMG_TGT} with BAPVERSION=${BAPVERSION}"
 sudo docker build . -f Dockerfile \
      -t  ${IMG_TGT} \
-     ${DOCKER_BLD_ARGS} \
+     --build-arg SWITCH=${OPAMSWITCH} \
      --build-arg BAPVERSION=${BAPVERSION} 
 
 docker tag superset_disasm:${TAG}-${FSUFFIX} superset_disasm:latest-${FSUFFIX}
