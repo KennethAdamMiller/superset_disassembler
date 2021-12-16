8aad2c3eb66f7832a83118f0936526853d1d3634
Kenneth Miller <kennethadammiller@gmail.com>
2021-12-16 18:14:19 -0500

    Add new project manifest and untagLatest scripts

---
+++ scripts/project.sh
@@ -0,0 +1,6 @@
+PROJECT=superset_disasm
+PROJ_IMG=${PROJECT}
+OPAM_PROJ=$(echo ${PROJECT} | tr '_' '-' | tr [:upper:] [:lower:])
+#DESCRIPTION=
+#AUTHOR_NAME
+#AUTHOR_EMAIL
---
+++ scripts/untagLatest.sh
@@ -0,0 +1,5 @@
+source scripts/tag.sh
+source scripts/feature_suffix.sh
+source scripts/project.sh
+
+docker rmi ${PROJ_IMG}:${TAG}-${FSUFFIX}
