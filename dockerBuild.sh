d95ae1aa40a4513a0486bcf97b9d7fbfd2487bb5
Kenneth Miller <kennethadammiller@gmail.com>
2021-12-16 20:09:23 -0500

    Use different image check cmd

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -7,8 +7,7 @@ source scripts/feature_suffix.sh
 source vars.sh
 IMG_TGT=superset_disasm:${TAG}-${FSUFFIX}
 
-docker manifest inspect $IMG_TGT > /dev/null ;
-if [[ $? == 1 ]]; then
+if [[ "$(docker images -q superset_disasm:latest-${FSUFFIX} 2> /dev/null)" == "" ]]; then
 	echo "Building ${IMG_TGT} with BAPVERSION=${BAPVERSION}"
 	sudo docker build . -f Dockerfile \
 	     -t  ${IMG_TGT} \
