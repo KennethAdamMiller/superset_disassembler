c875f794402c9963a82af16a9b0bc22af7eff641
Kenneth Miller <kennethadammiller@gmail.com>
2021-12-30 18:06:35 -0500

    Refactor corpora analysis to use deploy

--- scripts/corpora_analysis.sh
+++ scripts/corpora_analysis.sh
@@ -1,10 +1,5 @@
-source scripts/tag.sh
-source scripts/feature_suffix.sh
 export test_size=999999
-export cores=90
-cat ./configs/broker-job.yaml | envsubst | kubectl apply -f -
-cat ./configs/broker-service.yaml | envsubst | kubectl apply -f -
-cat ./configs/analysis-deployment.yaml | envsubst | kubectl apply -f -
+./scripts/deploy.sh
 kubectl wait --for=condition=complete --timeout=120s job/analysis-${TAG}-${FSUFFIX}
 kubectl wait --for=condition=complete --timeout=120s job/broker-${TAG}-${FSUFFIX}
 if [[ $? -eq 0 ]]; then
